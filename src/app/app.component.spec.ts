2d6588fd7d009cdaef75a86539d84665762109aa
supernova90 <majdiabbasjob@gmail.com>
2023-06-13 09:33:41 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wild-app'`, () => {
+  it(`should have as title 'my-dream-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wild-app');
+    expect(app.title).toEqual('my-dream-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wild-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-dream-app app is running!');
   });
 });
